088e9de60376994c91306bdcaa36a304bf7b4b24
alwil <alexandra.wilczynska@gmail.com>
2022-11-17 17:04:09 +0100

    gitautosync update 25

--- scripts/intro-to-r.R
+++ scripts/intro-to-r.R
@@ -103,5 +103,8 @@ year_country_gdp <- select(gapminder, year, country, gdpPercap)
 str(year_country_gdp)
 
 
-
+year_country_gdp <- gapminder %>%
+  select(year, country, gdpPercap)
+ 
+str(year_country_gdp)   
 

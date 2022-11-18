af688b656106ce1ffc3a3f7e3a8300fdac18a635
Claudiu Forgaci <claudiuforgaci@gmail.com>
2022-11-18 15:39:50 +0100

    gitautosync update 35

--- scripts/vector.R
+++ scripts/vector.R
@@ -174,7 +174,8 @@ leisure_colors <- rainbow(15)
 ggplot() +
   geom_sf(data = boundary_Delft, fill = "grey", colour = "grey") +
   geom_sf(data = lines_Delft_selection, aes(color = highway), size = 1) +
-  geom_sf(data = point_Delft, aes(fill = leisure), shape = 21) +
+  geom_sf(data = point_Delft, aes(fill = leisure), shape = 22) +
+  scale_color_manual(values = road_colors, name = "Road Types") +
   scale_fill_manual(values = leisure_colors, name = "Leisure Locations") +
   ggtitle("Mobility networks in Delft with leisure locations") +
   coord_sf(datum = st_crs(28992))

9749861664736ccb6452529fc50e153fc937047e
alwil <alexandra.wilczynska@gmail.com>
2022-11-18 11:16:33 +0100

    gitautosync update 46

--- scripts/intro-to-r.R
+++ scripts/intro-to-r.R
@@ -94,6 +94,14 @@ summary(gapminder)
 
 # select 
 
+year_country_gdp <- select(gapminder, year, country, gdpPercap)
 
+head(year_country_gdp)
 
+#%>% 
+# #|>  
+
+gapminder %>%
+  select(year, country, gdpPercap)
+  
 

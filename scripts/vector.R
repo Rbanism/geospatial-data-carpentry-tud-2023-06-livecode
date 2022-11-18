2a0c8a34ffee2221a57ff4c54455654edd37fe20
Claudiu Forgaci <claudiuforgaci@gmail.com>
2022-11-18 13:45:52 +0100

    gitautosync update 10

--- scripts/vector.R
+++ scripts/vector.R
@@ -15,8 +15,10 @@ st_crs(boundary_Delft)
 
 st_bbox(boundary_Delft)
 
-ggplot(data = boundary_Delft) %>% 
+ggplot(data = boundary_Delft) + 
+  geom_sf(size = 3, color = "black", fill = "cyan1") +
+  ggtitle("Delft Administrative Boundary") +
+  coord_sf(datum = st_crs(28992))
 
-
-
+# Challenge 1
 

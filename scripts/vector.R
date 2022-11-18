f3df87d73babea1294bcbb654747ac39755b7337
Claudiu Forgaci <claudiuforgaci@gmail.com>
2022-11-18 14:41:17 +0100

    gitautosync update 22

--- scripts/vector.R
+++ scripts/vector.R
@@ -126,4 +126,5 @@ road_colors <- c("blue", "green", "navy", "purple")
 
 ggplot(data = lines_Delft_selection) +
   geom_sf(aes(color = highway)) +
-  scale_color_manual(values = road_colors)
+  scale_color_manual(values = road_colors) +
+  coord_sf(datum = st_crs(28992))

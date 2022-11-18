2830d304b64a049d9b4e23bc080c87961db7debd
Claudiu Forgaci <claudiuforgaci@gmail.com>
2022-11-18 13:41:29 +0100

    gitautosync update 9

--- scripts/vector.R
+++ scripts/vector.R
@@ -7,3 +7,16 @@ library(sf)
 
 boundary_Delft <- st_read(here("data", "delft-boundary.shp"))
 
+st_geometry_type(boundary_Delft)
+st_crs(boundary_Delft)
+
+boundary_Delft <- st_transform(boundary_Delft, 28992)
+st_crs(boundary_Delft)
+
+st_bbox(boundary_Delft)
+
+ggplot(data = boundary_Delft) %>% 
+
+
+
+

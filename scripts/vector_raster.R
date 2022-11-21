33e18815df4733d9478d25ae2352d24989d63f0d
clementinecttn <clementine.cottineau@iscpif.fr>
2022-11-21 13:31:08 +0100

    gitautosync update 2

--- scripts/vector_raster.R
+++ scripts/vector_raster.R
@@ -1,2 +1,13 @@
 #### ---- OSM data
 
+library(tidyverse)
+library(here)
+install.packages("osmextract")
+library(osmextract)
+library(sf)
+install.packages("remotes")
+library(remotes)
+remotes::install_github("ropensci/osmdata")
+library(osmdata)
+
+

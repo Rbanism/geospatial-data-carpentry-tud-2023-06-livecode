606c06a886c7e63d4f496bd4827918b0de7daf68
alwil <alexandra.wilczynska@gmail.com>
2023-06-02 13:53:29 +0200

    gitautosync update 12

--- data-carpentry/scripts/intro-to-r.R
+++ data-carpentry/scripts/intro-to-r.R
@@ -12,3 +12,8 @@ download.file("https://bit.ly/geospatial_data",
               here("data", "gapminder_data.csv"),
               mode = "wb")
 
+# R as calculator
+1 + 100
+
+# Variables and assignment
+

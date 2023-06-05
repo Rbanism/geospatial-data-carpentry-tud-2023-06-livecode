68e2292fb8f887a3a8b93322cbf52f870a1fab4b
Claudiu Forgaci <claudiuforgaci@gmail.com>
2023-06-05 13:39:43 +0200

    gitautosync update 7

--- data-carpentry/scripts/vector.R
+++ data-carpentry/scripts/vector.R
@@ -3,5 +3,13 @@ library(here)
 library(sf)
 
 # Open a shapefile
-
 boundary_Delft <- st_read(here("data", "delft-boundary.shp"))
+
+# Examine the metadata
+st_geometry_type(boundary_Delft)
+st_crs(boundary_Delft)
+st_bbox(boundary_Delft)
+
+boundary_Delft <- st_transform(boundary_Delft, 28992)
+st_crs()
+

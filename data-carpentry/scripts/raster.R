bf251f16b1d57baba86aeab3ba764c6a869ba20b
Claudiu Forgaci <claudiuforgaci@gmail.com>
2023-06-06 11:55:59 +0200

    gitautosync update 57

--- data-carpentry/scripts/raster.R
+++ data-carpentry/scripts/raster.R
@@ -145,3 +145,10 @@ ggplot() +
   scale_fill_gradientn(name = "Elevation", 
                        colors = terrain.colors(10)) +
   coord_quickmap()
+
+ggplot() +
+  geom_raster(data = DTM_hill_TUD_df,
+              aes(x = x, y = y,
+                  alpha = tud.dtm.5m.hill.ETRS89)) +
+  coord_quickmap()
+  

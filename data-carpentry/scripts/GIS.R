c07e90338c2692007efb86e9018749d714a3b9e0
clementinecttn <clementine.cottineau@iscpif.fr>
2023-06-06 14:40:03 +0200

    gitautosync update 10

--- data-carpentry/scripts/GIS.R
+++ data-carpentry/scripts/GIS.R
@@ -18,4 +18,12 @@ assign("has_internet_via_proxy", TRUE, environment(curl::has_internet))
 buildings <- x$osm_polygons %>%
   st_transform(., 28992)
 
-summary(buildings$start_date)
+buildings$build_date <- 
+ if_else(as.numeric(buildings$start_date) < 1900,
+         1900, as.numeric(buildings$start_date))
+
+ggplot(data = buildings) +
+  geom_sf(aes(fill = build_date, 
+              colour = build_date)) +
+  scale_fill_viridis_c(option = "viridis") +
+  scale_colour_viridis_c(option = "viridis")

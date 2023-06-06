77b79280ad9eeacc1bfb5e292d766b187d68ae30
clementinecttn <clementine.cottineau@iscpif.fr>
2023-06-06 14:13:30 +0200

    gitautosync update 6

--- data-carpentry/scripts/GIS.R
+++ data-carpentry/scripts/GIS.R
@@ -8,3 +8,9 @@ bb
 
 bbnl <- getbb("Brielle, NL", format_out = "data.frame")
 bbnl
+
+x <- opq(bbox = bbnl) %>%
+  add_osm_feature(key = "building") %>%
+  osmdata_sf()
+
+assign("has_internet_via_proxy", TRUE, environment(curl::has_internet))
